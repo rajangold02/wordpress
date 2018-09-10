8886bdbda5d0517d2dcecb2920feefd08ffbcc35
rajangold02 <rajangold02@gmail.com>
2018-09-10 02:34:46 -0400

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+  region     = "us-east-1"
 }
 terraform {
   backend "s3" {
